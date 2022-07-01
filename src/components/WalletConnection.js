710af8e894f75bd73a394ac6df3c597a74ef0597
Andrey Ezhov <rybakisa@gmail.com>
2022-07-02 00:05:22 +0400

    Connect TON Wallet
    Add silly TON Wallet Plugin integration

--- src/components/WalletConnection.js
+++ src/components/WalletConnection.js
@@ -1,7 +1,50 @@
 import React from 'react'
 import TonWeb from "tonweb";
 
-console.log('tonready');
+// Use tonweb
+const tonweb = new TonWeb();
+console.log('start execution');
+
+// Use extension
+const onTonReady = () => {
+    console.log('tonready');
+
+    if (!window.tonProtocolVersion || window.tonProtocolVersion < 1) {
+        alert('Please update your TON Wallet Extension');
+        return;
+    }
+
+    const provider = window.ton;
+    console.log('isTonWallet=', provider.isTonWallet);
+
+    const connect = async () => {
+        // Request account access if needed
+        const accounts = await provider.send('ton_requestAccounts');
+        // Accounts now exposed, use them
+        const account = accounts[0] // We currently only ever provide a single account,
+                                    // but the array gives us some room to grow.
+        showAccountAddress(account);
+
+        console.log(await provider.send('ton_requestWallets'));
+    }
+
+    provider.on('accountsChanged', function (accounts) {
+        console.log('accountsChanged', accounts);
+        const account = accounts[0];
+        showAccountAddress(account);
+    });
+
+    function showAccountAddress(address) {
+        document.getElementById('address').innerText = address;
+    }
+}
+
+// Initialize TON
+if (window.ton) {
+    onTonReady();
+} else {
+    console.log('no ton :(')
+}
 
 const WalletConnection = () => {
   return (
@@ -9,7 +52,7 @@ const WalletConnection = () => {
       <h1>TON dApp Example</h1>
 
       <div class="grey">
-          <span>My address is</span>
+          <span>My address is </span>
           <span id="address"></span>
       </div>
     </>
