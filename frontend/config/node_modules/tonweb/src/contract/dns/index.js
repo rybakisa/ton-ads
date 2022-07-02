6ecfb67d11340daa7c175e69d43abadc352e7d0f
Andrey Ezhov <rybakisa@gmail.com>
2022-07-02 16:58:08 +0400

    Remove copy of node_modules folder

--- frontend/config/node_modules/tonweb/src/contract/dns/index.js
+++
@@ -1,7 +0,0 @@
-const Dns = require("./Dns").default;
-const {DnsCollection} = require("./DnsCollection");
-const {DnsItem} = require("./DnsItem");
-
-module.exports.default = {
-    Dns, DnsCollection, DnsItem
-};
